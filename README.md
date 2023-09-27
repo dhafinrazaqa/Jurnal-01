7a7990b065aafb24c8e739398ec0ac9e75eb3e57
dhafinrazaqa <dhafin.razaqa12@gmail.com>
2023-09-27 09:21:19 +0700

    Jurnal 01

---
+++ README.md
@@ -0,0 +1,3 @@
+Nama: Dhafin Razaqa Lesmana Putra
+NIM: 607062300030
+Kelas: D3IF-47-04
---
+++ jurnal01.java
@@ -0,0 +1,7 @@
+public class jurnal01 {
+    public static void main(String[] args) {
+        System.out.println("Nama: Dhafin Razaqa Lesmana Putra");
+        System.out.println("NIM: 607062300030");
+        System.out.println("Kelas: D3IF-47-04");
+    }
+}
